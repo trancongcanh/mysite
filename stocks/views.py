5e3d6c640870a7c710d2e6dcbd59f75ea82cc276
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-09 13:19:21 +0700

    sort

--- stocks/views.py
+++ stocks/views.py
@@ -15,9 +15,10 @@ def index(request):
         latest_companys = []
         for company in latest_company_list:
             for company in latest_company_list:
-                if (company.efficiency_level == latest_company_list_sort_laster(0)):
-                    latest_companys.append(company)
-                    latest_company_list_sort_laster.pop(0)
+                if latest_company_list_sort_laster :
+                    if (company.efficiency_level == latest_company_list_sort_laster[0]):
+                        latest_companys.append(company)
+                        del latest_company_list_sort_laster[0]
         template = loader.get_template('stocks/index.html')
         context = {
             'latest_company_list': latest_companys,
