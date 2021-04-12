b6862135ce4bf4a6c2496ffc76c34603f1e9c912
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-12 10:32:24 +0700

    search

--- stocks/views.py
+++ stocks/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from datetime import datetime
+
 
 from .models import Company
  
@@ -23,25 +25,52 @@ def search(request):
     try:
         company_capital = request.POST['company_cap']
         count = request.POST['count']
+        date_update = request.POST['date_update']
         company_capital_view=''
         count_record_view=''
         latest_company_list_view = []
         latest_company_list = []
-        if (company_capital != ""):
-            company_capital_view=company_capital
-            if (count !=""):
-                count_record_view=count
-                count_record = int(count)
-                latest_company_list_view = Company.objects.filter(company_cap=company_capital).order_by('efficiency_level')[:count_record]
+        date_update_view = ""
+        date_update_view_list = date_update.split("/")
+        for index in range(len(date_update_view_list)):
+            if index != 0 :
+                date_update_view = date_update_view_list[index] + "-" + date_update_view
+            else: 
+                date_update_view = date_update_view_list[index] + date_update_view
+        date_update_view = datetime.strptime(date_update_view, "%Y-%m-%d")
+        if (date_update != ""):
+            if (company_capital != ""):
+                company_capital_view=company_capital
+                if (count !=""):
+                    count_record_view=count
+                    count_record = int(count)
+                    latest_company_list_view = Company.objects.filter(company_cap=company_capital, date_update=date_update_view).order_by('efficiency_level')[:count_record]
+                else:
+                    latest_company_list_view = Company.objects.filter(company_cap=company_capital, date_update=date_update_view).order_by('efficiency_level')
             else:
-                latest_company_list_view = Company.objects.filter(company_cap=company_capital).order_by('efficiency_level')
+                if (count !=""):
+                    count_record = int(count)
+                    count_record_view=count
+                    latest_company_list_view = Company.objects.filter(date_update=date_update_view).order_by('efficiency_level')[:count_record]
+                else:
+                    latest_company_list_view = Company.objects.filter(date_update=date_update_view).order_by('efficiency_level')
         else:
-            if (count !=""):
-                count_record = int(count)
-                count_record_view=count
-                latest_company_list_view = Company.objects.all().order_by('efficiency_level')[:count_record]
+            if (company_capital != ""):
+                company_capital_view=company_capital
+                if (count !=""):
+                    count_record_view=count
+                    count_record = int(count)
+                    latest_company_list_view = Company.objects.filter(company_cap=company_capital).order_by('efficiency_level')[:count_record]
+                else:
+                    latest_company_list_view = Company.objects.filter(company_cap=company_capital).order_by('efficiency_level')
             else:
-                latest_company_list_view = Company.objects.all().order_by('efficiency_level')
+                if (count !=""):
+                    count_record = int(count)
+                    count_record_view=count
+                    latest_company_list_view = Company.objects.all().order_by('efficiency_level')[:count_record]
+                else:
+                    latest_company_list_view = Company.objects.all().order_by('efficiency_level')
+
         list_tmp = []
         for company in latest_company_list_view:
             list_tmp = [company]
@@ -51,6 +80,7 @@ def search(request):
         len_company = len(latest_company_list)
         template = loader.get_template('stocks/index.html')
         context = {
+            'date_update_view': date_update,
             'len_company': len_company,
             'count_record_view': count_record_view,
             'company_capital_view': company_capital,
@@ -73,13 +103,21 @@ def search(request):
                 count_record = int(count)
             except ValueError:
                 message2 = "Số record chỉ chứa số half size."
+        if (date_update !=""):
+            date_update_view_error = date_update  
+            try:
+                date_update_view_error = datetime.strptime(date_update_view, "%Y-%m-%d")
+            except ValueError:
+                message3 = "Ngày tìm kiếm sai định dạng."        
         latest_company_list=[]
         template = loader.get_template('stocks/index.html')
         context = {
+                    'date_update_view': date_update,                    
                     'count_record_view': count_record_view,
                     'company_capital_view': company_capital,
                     'message': message,
                     'message2': message2,
+                    'message3': message3,
                     'latest_company_list': latest_company_list,
                 }
     return HttpResponse(template.render(context, request))
