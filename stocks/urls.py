7aec85c640bbc0228b1fdf0f671ac1b4a1cd8daa
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-16 17:29:28 +0700

    search

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url 
- 
+from django.contrib.auth import views as auth_views
+
 from . import views 
 
 app_name = "stocks"
@@ -7,4 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search, name='search'),
     url(r'^upload-csv/', views.profile_upload, name="profile_upload"),
+    url(r'^login/',auth_views.LoginView.as_view(template_name="stocks/login.html"), name="login"),
+    url(r'^logout/',auth_views.LogoutView.as_view(next_page='/stocks/'),name='logout'),
 ]
