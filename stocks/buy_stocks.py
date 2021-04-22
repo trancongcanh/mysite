06bb62f2eb5b24b8d56ae816347446dfa1a79202
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-22 13:44:57 +0700

    sell/buy

--- stocks/buy_stocks.py
+++ stocks/buy_stocks.py
@@ -10,7 +10,7 @@ def buy_stocks(request):
     # Get dữ liệu từ session
     username=request.session.get('member_id', '')
     # Get stocks từ request
-    stocks = request.POST.get('stocks', '')
+    stock = request.POST.get('stock', '')
     # Thực hiện xóa các điều kiện tìm kiếm (nếu có) ở MH danh sách hiện tại trên session
     if request.session.get('company_cap','') != "":
         del request.session['company_cap'] 
@@ -21,17 +21,16 @@ def buy_stocks(request):
     # Lấy ra data từ CSDL để hiển thị ở MH danh sách
     company_list_db = Company.objects.order_by('-efficiency_level')  
     list_stocks = []
-    for company in company_list_db:
-        list_stocks.append(company.stocks)  
     template = loader.get_template('stocks/buy_stock.html')
     current_price = ""
-    if stocks != '':
-        for company in company_list_db:
-            if company.stocks == stocks:
-                current_price = company.current_price
+    for company in company_list_db:
+        if company.stocks == stock:
+            current_price = company.current_price
+        else:
+            list_stocks.append(company.stocks)  
     context = {
         'current_price': current_price,
         'list_stocks': list_stocks,
-        'stock': stocks
+        'stock': stock
     }
     return HttpResponse(template.render(context, request))
