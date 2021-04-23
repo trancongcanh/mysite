09163b0b185e6f57962770d67514c9e3d9f22300
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-23 14:13:39 +0700

    sell/buy

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,7 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi-VN'
+# LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
