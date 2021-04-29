8ad138e397918f5d4d7c13c1e473d639d2e2529a
Trần Công Cảnh <trancongcanh@luvina.net>
2021-04-29 13:55:23 +0700

    sell/buy

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,9 +130,9 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
